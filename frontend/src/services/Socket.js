41aa97ea5af438f016f3cc5340599e5f7569c6da
sriramrknp <sriramrknp82@gmail.com>
2025-02-07 18:50:07 +0530

    updated-import-socket

--- frontend/src/services/Socket.js
+++ frontend/src/services/Socket.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_BACKEND_URL, {
+const Socket = io(process.env.REACT_APP_BACKEND_URL, {
   withCredentials: true,
   transports: ['websocket', 'polling'], // Enable both transports
 });
 
-export default socket;
+export default Socket;
